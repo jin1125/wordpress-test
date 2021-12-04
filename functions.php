e2d8a876651a4292ceb4d3d724e72959eaf350b5
jin1125 <shogo.jinta@gmail.com>
2021-12-04 18:40:30 +0900

    2

--- functions.php
+++ functions.php
@@ -2,9 +2,17 @@
 add_action('init', function () {
   add_theme_support('title-tag');
   add_theme_support('post-thumbnails');
+  register_post_type('info', [
+    'label' => 'お知らせ',
+    'public' => true,
+    'menu_position' => 5,
+    'supports' => ['thumbnail', 'title', 'editor', 'page-attributes'],
+    'has_archive' => true,
+    'show_in_rest' => true
+  ]);
 });
 
 add_action('wp_enqueue_scripts', function () {
-  wp_enqueue_style('reset', get_template_directory_uri().'/reset.css');
+  wp_enqueue_style('reset', get_template_directory_uri() . '/reset.css');
   wp_enqueue_style('style', get_stylesheet_uri(), array('reset'));
 });
--- home.php
+++ home.php
@@ -1,3 +1,32 @@
 <?php get_header(); ?>
-<h1>test</h1>
+<!-- ------------------------------ -->
+<!-- <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <h1><?php the_title(); ?></h1>
+    <?php the_content(); ?>
+    <br>
+<?php endwhile;
+      endif; ?> -->
+
+<!-- ------------------------------ -->
+<?php
+$args = array(
+  'post_type' => 'info',
+  'posts_per_page' => 3,
+  'paged' => $paged
+);
+$the_query = new WP_Query($args);
+?>
+<div class="container">
+  <?php if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post(); ?>
+      <div><?php echo get_the_date('Y.m.d'); ?></div>
+      <h2><?php the_title(); ?></h2>
+      <?php the_content(); ?>
+      <br>
+    <?php endwhile; ?>
+</div>
+<?php wp_reset_postdata(); ?>
+<?php else : ?>
+<?php endif; ?>
+
+<!-- ------------------------------ -->
 <?php get_footer(); ?>
