26670ecdd860b4ff0b918facfc5c4ddf8bbc40e6
jin1125 <shogo.jinta@gmail.com>
2021-12-10 07:49:47 +0900

    update

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ document.querySelector(".menu-btn").addEventListener("click", function () {
 
 window.addEventListener("scroll", function () {
   // ヘッダーを変数の中に格納する
-  const header = document.getElementById('fixed');
+  const header = document.getElementById("fixed");
   // 100px以上スクロールしたらヘッダーに「scroll-nav」クラスをつける
   header.classList.toggle("scroll-nav", window.scrollY > 100);
 });
